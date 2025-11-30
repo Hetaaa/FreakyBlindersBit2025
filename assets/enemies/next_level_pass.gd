67b9118cd93ae2fb0ccd5d490485479096cc080d
patryktkaczewskiii <patryktkaczewskiii@gmail.com>
2025-11-30 04:47:20 +0100

    kills count

--- assets/enemies/next_level_pass.gd
+++ assets/enemies/next_level_pass.gd
@@ -1,16 +1,12 @@
 extends Node
-
-var current_scene = get_tree().current_scene
+var kills = Global.kills
+var current_scene = get_tree().current_scene.scene_file_path
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-func kills_pass() -> void:
-	if Global.kills==1 and current_scene.name==World3D:
-		get_tree().change_scene_to_file("res://assets/OurAssets/Maps/Bar/BarScene.tscn")
 	
--- scripts/OurGame/global.gd
+++ scripts/OurGame/global.gd
@@ -6,12 +6,19 @@ var kills : int = 0
 var player : Player
 
 var time_freeze :bool = false
-
+var current_scene = get_tree().current_scene.scene_file_path
 var just_unfreezed : bool = false
 var cutscene : bool = false
-
+func _process(delta: float) -> void:
+	kills_pass()
+	
 func toggle_freeze():
 	time_freeze = !time_freeze
 	just_unfreezed = true
 	highlight_changed.emit(time_freeze)
 #new
+func kills_pass() -> void:
+	if kills<10 and current_scene=="res://assets/OurAssets/Maps/Bar/BarScene.tscn":
+		#get_tree().change_scene_to_file()
+		print("Kills =", kills)
+	
